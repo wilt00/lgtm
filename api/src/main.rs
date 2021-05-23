25c04c96a603a16dfb7f0a4a0ca45f01124eee03
Bharat Middha <5100938+bmiddha@users.noreply.github.com>
2021-05-22 22:45:54 -0400

    :crown: pimp my api

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,25 +1,46 @@
-use lgtm;
+use actix_web::{get, App, HttpRequest, HttpServer};
+use lgtm::lgtm;
+use serde::Deserialize;
 
-use std::env;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
-
-async fn index<'a>() -> impl Responder {
-    HttpResponse::Ok().body(lgtm::lgtm() + "\n")
+fn default_port() -> u16 {
+    8088
 }
 
-#[actix_rt::main]
+#[derive(Deserialize, Debug)]
+struct Config {
+    #[serde(default = "default_port")]
+    port: u16,
+}
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> String {
+    lgtm() + "\n"
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let default_port = String::from("8088");
-    let port = env::var("PORT")
-        .unwrap_or_else(|_| default_port)
-        .parse()
-        .expect("PORT must be a number");
-    println!("Listening on port: {}", port);
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(index))
-    })
-    .bind(("0.0.0.0", port))?
-    .run()
-    .await
+    match envy::from_env::<Config>() {
+        Ok(config) => {
+            println!("Listening on port: {}", config.port);
+            HttpServer::new(|| App::new().service(index))
+                .bind(("0.0.0.0", config.port))?
+                .run()
+                .await
+        }
+        Err(error) => panic!("{:#?}", error),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{test, App};
+
+    #[actix_rt::test]
+    async fn test_index_get() {
+        let mut app = test::init_service(App::new().service(index)).await;
+        let req = test::TestRequest::with_header("content-type", "text/plain").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
 }
